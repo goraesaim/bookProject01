7d4dad73dc7c950604e17a98a1ab739767c8ba50
DESKTOP-ROO9DKH\it <applause000000@gmail.com>
2025-11-04 13:15:53 +0900

    book-list 연결

--- src/main/java/com/parket/webproject/controller/controller/domestic/DomesticController.java
+++ src/main/java/com/parket/webproject/controller/controller/domestic/DomesticController.java
@@ -1,15 +1,22 @@
 package com.parket.webproject.controller.controller.domestic;
 
+import com.parket.webproject.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/domestic")
 public class DomesticController {
+    @Autowired
+    private BookRepository bookRepository;
+
 
     @GetMapping("/list")
-    public String showDomesticList() {
+    public String showDomesticList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
         return "domestic/list";  // templates/domestic/list.html
     }
     @GetMapping("/detail")
