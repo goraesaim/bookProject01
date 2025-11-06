443dc49fde10eae3948d27b80b67169bfb3fdb73
DESKTOP-ROO9DKH\it <applause000000@gmail.com>
2025-11-06 18:08:43 +0900

    중고 등록시 크롤링 매칭 기능 추가

--- src/main/java/com/parket/webproject/controller/ProductController.java
+++ src/main/java/com/parket/webproject/controller/ProductController.java
@@ -1,18 +1,18 @@
 package com.parket.webproject.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parket.webproject.dto.BookDTO;
 import com.parket.webproject.dto.ProductDTO;
+import com.parket.webproject.service.BookService;
 import com.parket.webproject.service.ProductService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -21,6 +21,8 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private BookService bookService;
 
     @GetMapping("/write")
     public void registerGet() {
@@ -73,6 +75,17 @@ public class ProductController {
         List<ProductDTO> products = productService.findAllProducts();
         model.addAttribute("products", products);
     }
+
+    //Taek 추가본
+    @GetMapping("/select")
+    @ResponseBody
+    public List<BookDTO> submitMessage(@RequestParam("message") String message) throws IOException {
+        List<BookDTO> books = bookService.CrawlSelectBook(message);
+        return books;
+    }
+
+
+
 //
 //    // 상품 상세 조회 및 수정 페이지
 //    @GetMapping({"/read", "/modify"})
